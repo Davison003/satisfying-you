d0b526eee97f0500c7ef5143ac155851536b4201
Davi <daviregonatti@gmail.com>
2024-11-05 13:42:56 -0300

    style: change to suit given style

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,9 +1,13 @@
 import { StyleSheet, Image, TouchableOpacity, Text } from "react-native";
 
+const cardSize = [120, 185];
+const imgSize = [100, 100];
+
 export default function Card(info: any) {
+  // const img = require(`../assets/images/${info.image}`);
   return (
     <TouchableOpacity style={styles.container}>
-      <Image source={info.image} width={120} height={120} />
+      <Image source={info.image} alt={info.title} style={styles.imgStyle} />
       <Text style={styles.title}>{info.title}</Text>
       <Text style={styles.date}>{info.date}</Text>
     </TouchableOpacity>
@@ -14,20 +18,33 @@ const styles = StyleSheet.create({
   container: {
     display: "flex",
     flexDirection: "column",
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-    // padding: 16,
+    justifyContent: "space-between",
+
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 16,
+    width: cardSize[0],
+    height: cardSize[1],
   },
 
   title: {
     color: "#48a",
     fontSize: 18,
+    textAlign: "center",
     fontFamily: "sans-serif",
     textTransform: "uppercase",
   },
   date: {
     fontSize: 12,
     opacity: 0.5,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  imgStyle: {
+    width: imgSize[0],
+    height: imgSize[1],
+    borderRadius: 10,
+    marginBottom: 5,
   },
 });
