a3a42a5418296f6efa66a02c6aa85f5fbbf502e2
Davi <daviregonatti@gmail.com>
2024-11-05 21:52:38 -0300

    feat: add basic nav support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import Home from "./(tabs)/Home";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)/Home" />
+    </Stack>
+  );
+}
 
 // export default function RootLayout() {
-//   return <Stack>{/* <Stack.Screen name="index" /> */}</Stack>;
+//   return (
+//     <Stack screenOptions={{ headerShown: false }}>
+//       <Stack.Screen
+//         name="../views/Home"
+//         options={{ title: "Satisfying You" }}
+//       />
+//     </Stack>
+//   );
 // }
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
-import DrawerNav from "@/components/DrawerNav";
-import { Button } from "react-native-paper";
-
-import Home from "../views/Home";
-
-const mainStyles = StyleSheet.create({
-  bg: {
-    backgroundColor: "purple",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import * as React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+import Home from "./(tabs)/Home";
 
 export default function Index() {
   return <Home />;
